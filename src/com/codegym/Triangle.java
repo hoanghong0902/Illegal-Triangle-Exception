0d8120e47458b6c614d70bd171fb856818e8cd07
HoangHong <piiyk172@gmail.com>
2023-05-30 15:39:27 +0700

    First commit

---
+++ src/com/codegym/Triangle.java
@@ -0,0 +1,24 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hãy nhập x: ");
+        int x = scanner.nextInt();
+        System.out.println("Hãy nhập y: ");
+        int y = scanner.nextInt();
+        System.out.println("Hãy nhập x: ");
+        int z = scanner.nextInt();
+        illegalTriangleException(x,y,z);
+    }
+    private static void illegalTriangleException(int x, int y, int z){
+        if ((x + y > z) && (x + z > y) && (y + z > x)
+            && x > 0 && y > 0 && z > 0){
+            System.out.println("Day la 3 canh cua tam giac");
+        } else {
+            throw new RuntimeException("Day khong phai la 3 canh cua tam giac");
+        }
+    }
+}
